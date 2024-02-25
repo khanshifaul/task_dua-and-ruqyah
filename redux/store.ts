ea6c70877eff052e10c9279562eefd61e1b344cb
khanshifaul <khanshifaul@gmail.com>
2024-02-25 17:36:08 +0600

    deleted:    redux/store.ts

--- redux/store.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers, createStore } from "redux";
-import { navigationReducer } from "./reducers/navigationReducer";
-
-const rootReducer = combineReducers({
-  navigation: navigationReducer,
-});
-
-export const store = createStore(rootReducer);
